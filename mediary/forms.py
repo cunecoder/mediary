5d299690ea0cee5c1725c92576d42d5009231fd3
SilasEC <silas.curtis@cune.org>
2025-04-28 21:56:14 -0500

    STYLED

--- mediary/forms.py
+++ mediary/forms.py
@@ -18,9 +18,30 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'location', 'description', 'time']
+        # widgets = {
+        #     'title': forms.TextInput(attrs={'placeholder': "Event Title..."}),
+        #     'location': forms.TextInput(attrs={'placeholder': "Location..."}),
+        #     'description': forms.Textarea(attrs={'placeholder': "Tell us about it!", 'rows': 3, 'cols': 30}),
+        #     'time': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+        # }
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': "Event Title..."}),
-            'location': forms.TextInput(attrs={'placeholder': "Location..."}),
-            'description': forms.Textarea(attrs={'placeholder': "Tell us about it!", 'rows': 3, 'cols': 30}),
-            'time': forms.DateTimeInput(attrs={'type': 'datetime-local'})
+            'title': forms.TextInput(attrs={
+                'placeholder': "Event Title...",
+                'class': 'form-control my-custom-class',
+                'id': 'event-title-input'
+            }),
+            'location': forms.TextInput(attrs={
+                'placeholder': "Location...",
+                'class': 'form-control',
+            }),
+            'description': forms.Textarea(attrs={
+                'placeholder': "Tell us about it!",
+                'rows': 3,
+                'cols': 30,
+                'class': 'form-control',
+            }),
+            'time': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control',
+            })
         }
